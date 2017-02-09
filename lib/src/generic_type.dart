91c64fbfe33e35625b40cf90abf4326ffc253657
Don <don.j.olmstead@gmail.com>
2017-02-08 21:44:31 -0800

    feat(type): Add generic type

---
+++ lib/src/generic_type.dart
@@ -0,0 +1,38 @@
+// Copyright (c) 2017 the Craft Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+//---------------------------------------------------------------------
+// Imports
+//---------------------------------------------------------------------
+
+import 'dart_type.dart';
+import 'interface_type.dart';
+import 'named_type.dart';
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+/// A type that references a generic.
+class GenericType extends DartType implements NamedType {
+  //---------------------------------------------------------------------
+  // Member variables
+  //---------------------------------------------------------------------
+
+  @override
+  final String name;
+
+  /// The interface that the generic extends from.
+  final InterfaceType extending;
+
+  //---------------------------------------------------------------------
+  // Construction
+  //---------------------------------------------------------------------
+
+  /// Creates a new [GenericType] with the given [name].
+  ///
+  /// If [extending] is provided the generic will always be of that type.
+  GenericType(this.name, [this.extending]);
+}
