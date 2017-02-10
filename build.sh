4d03c78e60657318a86f4a33f0b6d2083893d68e
Don <don.j.olmstead@gmail.com>
2017-02-09 22:15:49 -0800

    chore(drone): Hook in Drone CI

---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+set -ex
+
+# Clone unpublished craft-dart packages
+git clone https://github.com/craft-dart/craft-type.git ../craft-type
+
+# Get version
+dart --version
+
+# Get dependencies
+pub install
+
+# Verify that the libraries are error and warning-free.
+dartanalyzer ${DARTANALYZER_FLAGS} $(ls -rt lib/*.dart)
+
+# Run the tests
+pub global activate coverage
+OBSERVATORY_PORT=8000
+COVERAGE_OUTPUT=coverage.json
+
+dart --checked --observe=${OBSERVATORY_PORT} test/all.dart & \
+pub global run coverage:collect_coverage \
+    --port=${OBSERVATORY_PORT} \
+    --out ${COVERAGE_OUTPUT} \
+    --wait-paused \
+    --resume-isolates & \
+wait
+
+pub global run coverage:format_coverage \
+    --packages=.packages \
+    --report-on lib \
+    --in ${COVERAGE_OUTPUT} \
+    --out lcov.info \
+    --lcov
