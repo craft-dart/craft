6df6ebf2f18251e9787554d542b145177410526d
Don <don.j.olmstead@gmail.com>
2017-02-09 23:16:09 -0800

    feat(element): Create base element for common interface implementations

---
+++ lib/src/interfaces.dart
@@ -0,0 +1,12 @@
+// Copyright (c) 2017, the Craft Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+//---------------------------------------------------------------------
+// Exports
+//---------------------------------------------------------------------
+
+export 'interfaces/annotated.dart';
+export 'interfaces/commented.dart';
+export 'interfaces/named.dart';
