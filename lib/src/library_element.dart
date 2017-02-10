9bbb1961a4bfff69f7f43a5dc9cf301026c00fa9
Don <don.j.olmstead@gmail.com>
2017-02-09 22:53:55 -0800

    feat(element): Flesh out library element with uris

--- lib/src/library_element.dart
+++ lib/src/library_element.dart
@@ -9,6 +9,8 @@
 
 import 'element.dart';
 import 'enclosing_element.dart';
+import 'uri_referenced_element.dart';
+import 'helpers.dart';
 
 //---------------------------------------------------------------------
 // Library contents
@@ -16,5 +18,64 @@ import 'enclosing_element.dart';
 
 /// An [Element] representing a Dart library.
 class LibraryElement extends Element with EnclosingElement {
+  //---------------------------------------------------------------------
+  // Member variables
+  //---------------------------------------------------------------------
 
+  /// The name of the element.
+  final String name;
+
+  /// The location of the library.
+  final Uri uri;
+
+  /// The import references for the library.
+  final List<UriReferencedElement> imports;
+
+  /// The export references for the library.
+  final List<UriReferencedElement> exports;
+
+  //---------------------------------------------------------------------
+  // Construction
+  //---------------------------------------------------------------------
+
+  /// Creates an instance of [LibraryElement] with the given [uri].
+  ///
+  /// The library can be given a [name]. This would correspond to a `library`
+  /// directive within the library. The library directive has been optional
+  /// so it is not required.
+  ///
+  ///     library foo;
+  ///
+  /// For referenced dart files the [imports] and [exports] can be specified.
+  LibraryElement(
+    this.uri, {
+    String name,
+    Iterable<UriReferencedElement> imports,
+    Iterable<UriReferencedElement> exports,
+  })
+      : name = name ?? '',
+        imports = defaultList<UriReferencedElement>(imports),
+        exports = defaultList<UriReferencedElement>(exports) {
+    // Use `this` to properly scope the value
+    encloseAll(this.imports);
+    encloseAll(this.exports);
+  }
+
+  //---------------------------------------------------------------------
+  // Properties
+  //---------------------------------------------------------------------
+
+  /// The libraries imported into this library.
+  ///
+  /// The values returned do not account for any configuration specific imports
+  /// and will only return the interface library.
+  Iterable<LibraryElement> get imported =>
+      imports.map<LibraryElement>((value) => value.library);
+
+  /// The libraries exported by this library.
+  ///
+  /// The values returned do not account for any configuration specific exports
+  /// and will only return the interface library.
+  Iterable<LibraryElement> get exported =>
+      exports.map<LibraryElement>((value) => value.library);
 }
