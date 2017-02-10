ed776aa9de5ea9370071860b386811a59a63141f
Don <don.j.olmstead@gmail.com>
2017-02-09 22:07:09 -0800

    test(*): Add test runner

---
+++ test/all.dart
@@ -0,0 +1,19 @@
+// Copyright (c) 2017, the Craft Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+// \TODO Remove this file after https://github.com/dart-lang/test/issues/36 is resolved.
+
+//---------------------------------------------------------------------
+// Imports
+//---------------------------------------------------------------------
+
+import 'package:test/test.dart';
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+/// Entry point for tests.
+void main() {}
