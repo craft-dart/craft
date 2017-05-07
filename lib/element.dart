a4a07962a0b8bb043b18a7d8422b40ede0755ae3
Don <don.j.olmstead@gmail.com>
2017-05-06 20:50:58 -0700

    feat(element): Expose elements

--- lib/element.dart
+++ lib/element.dart
@@ -8,3 +8,17 @@ library craft_element.element;
 //---------------------------------------------------------------------
 // Exports
 //---------------------------------------------------------------------
+
+export 'src/base_element.dart';
+export 'src/constructor_element.dart';
+export 'src/element.dart';
+export 'src/enclosing_element.dart';
+export 'src/function_element.dart';
+export 'src/interfaces.dart';
+export 'src/library_element.dart';
+export 'src/method_element.dart';
+export 'src/parameter_element.dart';
+export 'src/parameter_kind.dart';
+export 'src/typedef_element.dart';
+export 'src/uri_configuration_element.dart';
+export 'src/uri_referenced_element.dart';
