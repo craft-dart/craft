80a03a31f05fca3c24690a881cb08b72a266b9e6
Don <don.j.olmstead@gmail.com>
2017-05-09 23:59:29 -0700

    feat(builder): Add MethodElementBuilder

--- lib/element_builder.dart
+++ lib/element_builder.dart
@@ -13,6 +13,7 @@ export 'src/comment_buffer.dart';
 export 'src/element_builder.dart';
 export 'src/function_element_builder.dart';
 export 'src/invalid_element_error.dart';
+export 'src/method_element_builder.dart';
 export 'src/parameter_element_builder.dart';
 export 'src/typedef_element_builder.dart';
 export 'src/uri_configuration_element_builder.dart';
