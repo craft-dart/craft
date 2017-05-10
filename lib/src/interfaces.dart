05680afa440f5f9fa9686f80faa48c0f35c5d303
Don <don.j.olmstead@gmail.com>
2017-05-09 23:58:17 -0700

    feat(builder): Add AbstractBuilder

--- lib/src/interfaces.dart
+++ lib/src/interfaces.dart
@@ -7,6 +7,7 @@
 // Exports
 //---------------------------------------------------------------------
 
+export 'interfaces/abstract_builder.dart';
 export 'interfaces/annotated_builder.dart';
 export 'interfaces/commented_builder.dart';
 export 'interfaces/defaults_builder.dart';
