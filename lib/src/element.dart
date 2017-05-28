93d9f1434fb7d209f138256d72037f266e09906f
Don <don.j.olmstead@gmail.com>
2017-05-27 20:01:11 -0700

    feat(matcher): Add ElementMatcher

---
+++ lib/src/element.dart
@@ -0,0 +1,20 @@
+// Copyright (c) 2017 the Craft Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+//---------------------------------------------------------------------
+// Imports
+//---------------------------------------------------------------------
+
+import 'package:craft_element/element.dart';
+import 'package:test/test.dart';
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+/// Returns a matcher that checks for equality between [Element]s.
+///
+/// The metadata matcher is not able to check equality for annotations.
+Matcher elementEqual(Element expected) => null;
