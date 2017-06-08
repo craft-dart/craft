37e6ec5d7c30b604a8f4b24fada94658a755cf2e
Don <don.j.olmstead@gmail.com>
2017-06-07 22:18:27 -0700

    chore(drone): Remove coverage from build

--- build.sh
+++ build.sh
@@ -11,21 +11,4 @@ pub install
 dartanalyzer ${DARTANALYZER_FLAGS} $(ls -rt lib/*.dart)
 
 # Run the tests
-pub global activate coverage
-OBSERVATORY_PORT=8000
-COVERAGE_OUTPUT=coverage.json
-
-dart --checked --observe=${OBSERVATORY_PORT} test/all.dart & \
-pub global run coverage:collect_coverage \
-    --port=${OBSERVATORY_PORT} \
-    --out ${COVERAGE_OUTPUT} \
-    --wait-paused \
-    --resume-isolates & \
-wait
-
-pub global run coverage:format_coverage \
-    --packages=.packages \
-    --report-on lib \
-    --in ${COVERAGE_OUTPUT} \
-    --out lcov.info \
-    --lcov
+pub run test
