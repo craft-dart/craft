21f8e1a22b42984391f23143ca351b5940c12e3c
Don <don.j.olmstead@gmail.com>
2017-06-08 22:43:52 -0700

    feat(matcher): Add annotation tests

--- lib/src/interfaces.dart
+++ lib/src/interfaces.dart
@@ -9,6 +9,7 @@
 
 export 'interfaces/abstract_test.dart';
 export 'interfaces/accessible_test.dart';
+export 'interfaces/annotated_test.dart';
 export 'interfaces/constant_test.dart';
 export 'interfaces/defaults_test.dart';
 export 'interfaces/named_test.dart';
