9ae1bc515521ca64510050ff6b27745bdde4b714
Don <don.j.olmstead@gmail.com>
2017-06-17 12:19:03 -0700

    fix(analyzer): Remove unused code

--- lib/src/library_element_visitor.dart
+++
@@ -1,9 +0,0 @@
-
-import 'package:analyzer/dart/element/element.dart' as analyzer;
-import 'package:craft_element_builder/element_builder.dart';
-
-abstract class LibraryElementVisitor implements analyzer.ElementVisitor<ElementBuilder> {
-  LibraryElementBuilder visitLibraryElement(analyzer.LibraryElement element) {
-
-  }
-}
