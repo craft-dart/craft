a47b3832e05ae421496989d028bef0dcde5de64d
Don <don.j.olmstead@gmail.com>
2017-06-17 10:54:21 -0700

    feat(analyzer): Add analysis context creation

---
+++ lib/context.dart
@@ -0,0 +1,69 @@
+// Copyright (c) 2017 the Craft Project Authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a zlib license that can be found in
+// the LICENSE file.
+
+library craft_analyzer.context;
+
+//---------------------------------------------------------------------
+// Imports
+//---------------------------------------------------------------------
+
+// This file includes internal implementation from the analyzer
+// ignore_for_file: implementation_imports
+
+import 'package:analyzer/file_system/file_system.dart' hide File;
+import 'package:analyzer/file_system/physical_file_system.dart';
+import 'package:analyzer/source/package_map_resolver.dart';
+import 'package:analyzer/source/pub_package_map_provider.dart';
+import 'package:analyzer/src/dart/sdk/sdk.dart';
+import 'package:analyzer/src/generated/engine.dart';
+import 'package:analyzer/src/generated/java_io.dart';
+import 'package:analyzer/src/generated/source.dart';
+import 'package:analyzer/src/generated/source_io.dart';
+import 'package:cli_util/cli_util.dart';
+
+//---------------------------------------------------------------------
+// Library contents
+//---------------------------------------------------------------------
+
+/// Creates an analysis context for a project.
+///
+/// The [sdkPath] points to the installed location of the Dart SDK.
+AnalysisContext analysisContext({Uri sdkPath}) {
+  sdkPath ??= getSdkDir().uri;
+
+  final sdkFilePath = sdkPath.toFilePath();
+
+  // Setup the core dart libraries
+  JavaSystemIO.setProperty('com.google.dart.sdk', sdkFilePath);
+  final resourceProvider = PhysicalResourceProvider.INSTANCE;
+  final sdk = new FolderBasedDartSdk(
+      resourceProvider, resourceProvider.getFolder(sdkFilePath));
+
+  // Using the .packages file
+  final pubPackageMapProvider =
+      new PubPackageMapProvider(PhysicalResourceProvider.INSTANCE, sdk);
+
+  final packageMapInfo = pubPackageMapProvider.computePackageMap(
+      PhysicalResourceProvider.INSTANCE.getResource('.') as Folder);
+
+  // Create the resolvers
+  final resolvers = <UriResolver>[
+    new DartUriResolver(sdk),
+    new ResourceUriResolver(PhysicalResourceProvider.INSTANCE),
+    new PackageMapUriResolver(
+        PhysicalResourceProvider.INSTANCE, packageMapInfo.packageMap)
+  ];
+
+  // Set the analysis options
+  final options = new AnalysisOptionsImpl()
+    ..preserveComments = true
+    ..strongMode = true
+    ..analyzeFunctionBodies = false;
+
+  // Return the context
+  return AnalysisEngine.instance.createAnalysisContext()
+    ..analysisOptions = options
+    ..sourceFactory = new SourceFactory(resolvers);
+}
